34beefe18cbf1f67960d70e0be35cb59847a6b5d
GhostTonyRus <67839985+GhostTonyRus@users.noreply.github.com>
2023-03-20 23:10:57 +0300

    изменения в бд

--- main_site/backend_api/models.py
+++ main_site/backend_api/models.py
@@ -7,15 +7,15 @@ class Application(models.Model):
     phone_number = models.CharField(verbose_name="Номер телефона", max_length=11, help_text="+7 999 99 99 999")
     email = models.EmailField(verbose_name="Email", max_length=50, help_text="Email")
     # Выберите услугу
-    landing_page = models.BooleanField(verbose_name="Лендинг", default=False, help_text="Лендинг")
-    business_card_website = models.BooleanField(verbose_name="Сайт-визитка", default=False, help_text="Сайт-визитка")
-    corporate_website = models.BooleanField(verbose_name="Корпоративный сайт", default=False, help_text="Корпоративный сайт")
-    whim = models.BooleanField(verbose_name="Каприз", default=False, help_text="Каприз")
+    landing_page = models.CharField(verbose_name="Лендинг", max_length=10, help_text="Лендинг")
+    business_card_website = models.CharField(verbose_name="Сайт-визитка", max_length=10, help_text="Сайт-визитка")
+    corporate_website = models.CharField(verbose_name="Корпоративный сайт", max_length=10, help_text="Корпоративный сайт")
+    whim = models.CharField(verbose_name="Каприз", max_length=10, help_text="Каприз")
     # бюджет
-    less_than_five_hundred_thousand = models.BooleanField(verbose_name="Менее 500 Т/Р", default=False, help_text="Менее 500 Т")
-    less_than_million = models.BooleanField(verbose_name="Менее Мил", default=False, help_text="Менее Мил")
-    more_than_million = models.BooleanField(verbose_name="Более Мил", default=False, help_text="Более Мил")
-    arbitrary_option = models.BooleanField(verbose_name="Не знаю", default=False, help_text="Не знаю")
+    less_than_five_hundred_thousand = models.CharField(verbose_name="Менее 500 Т/Р", max_length=10, help_text="Менее 500 Т")
+    less_than_million = models.CharField(verbose_name="Менее Мил", max_length=10, help_text="Менее Мил")
+    more_than_million = models.CharField(verbose_name="Более Мил", max_length=10, help_text="Более Мил")
+    arbitrary_option = models.CharField(verbose_name="Не знаю", max_length=10, help_text="Не знаю")
     # о проекте
     about_project = models.TextField(verbose_name="О проекте", help_text="Опишите Задачу")
 
