2be99998817b27b1d04a21b152c2a7a643046112
GhostTonyRus <67839985+GhostTonyRus@users.noreply.github.com>
2023-03-23 22:40:21 +0300

    изменения в бд

--- main_site/backend_api/models.py
+++ main_site/backend_api/models.py
@@ -6,21 +6,15 @@ class Application(models.Model):
     full_name = models.CharField(verbose_name="ФИО", max_length=100, help_text="ФИО")
     phone_number = models.CharField(verbose_name="Номер телефона", max_length=11, help_text="+7 999 99 99 999")
     email = models.EmailField(verbose_name="Email", max_length=50, help_text="Email")
-    # Выберите услугу
-    landing_page = models.CharField(verbose_name="Лендинг", max_length=10, help_text="Лендинг")
-    business_card_website = models.CharField(verbose_name="Сайт-визитка", max_length=10, help_text="Сайт-визитка")
-    corporate_website = models.CharField(verbose_name="Корпоративный сайт", max_length=10, help_text="Корпоративный сайт")
-    whim = models.CharField(verbose_name="Каприз", max_length=10, help_text="Каприз")
+    # услуга
+    service = models.CharField(verbose_name="Услуга", max_length=10, help_text="Услуга")
     # бюджет
-    less_than_five_hundred_thousand = models.CharField(verbose_name="Менее 500 Т/Р", max_length=10, help_text="Менее 500 Т")
-    less_than_million = models.CharField(verbose_name="Менее Мил", max_length=10, help_text="Менее Мил")
-    more_than_million = models.CharField(verbose_name="Более Мил", max_length=10, help_text="Более Мил")
-    arbitrary_option = models.CharField(verbose_name="Не знаю", max_length=10, help_text="Не знаю")
+    budget = models.CharField(verbose_name="Бюджет", default=False, help_text="Бюджет")
     # о проекте
     about_project = models.TextField(verbose_name="О проекте", help_text="Опишите Задачу")
 
     def __str__(self):
-        return f"{self.full_name}"
+        return f"{self.pk} - {self.full_name}"
 
     class Meta:
         verbose_name = "Заявка"
