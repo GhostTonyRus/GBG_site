912ca973bccad24517124a4073d8979a5747c2a3
GhostTonyRus <67839985+GhostTonyRus@users.noreply.github.com>
2023-04-08 23:53:44 +0300

    добавлены настройки для статических файлов

--- main_site/main_site/settings.py
+++ main_site/main_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-6_n*k$q3i8fvlywlex(@0qxd-)nh*wl6y=w#3uylafqal*g5p3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ALLOWED_HOSTS = ["antonmf6.beget.tech", "www.antonmf6.beget.tech"]
 ALLOWED_HOSTS = []
 
 
@@ -88,14 +89,14 @@ WSGI_APPLICATION = 'main_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gbg_application',
-        'USER': 'root',
-        'PASSWORD': 'Rocestear200@!',
-        'HOST': '127.0.0.1',
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'gbg_application',
+        # 'USER': 'root',
+        # 'PASSWORD': 'Rocestear200@!',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': 3306,
     }
 }
 
@@ -133,8 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
